d8110425b2cba66838eee2b3584f8853202b0a63
godban <bogdan.plieshka@zattoo.com>
2020-10-21 23:37:01 +0200

    Access repository package.json

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const core = require('@actions/core');
+const github = require('@actions/github');
 
 // most @actions toolkit packages have async methods
 async function run() {
-  try {
-    core.info(`Hello world`);
-  } catch (error) {
-    core.setFailed(error.message);
-  }
+    try {
+        const github_token = core.getInput('github_token', {required: true});
+        const npm_token = core.getInput('npm_token', {required: true});
+
+        const octokit = new github.GitHub(github_token, {previews: ['flash', 'ant-man']});
+
+        const response = await octokit.request('GET /repos/{owner}/{repo}/package.json', {
+            owner: github.context.repo.owner,
+            repo: github.context.repo.repo,
+        });
+
+        console.log(response);
+    } catch (error) {
+        core.setFailed(error.message);
+    }
 }
 
 run();
