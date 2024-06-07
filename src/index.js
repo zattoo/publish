d0bcccc5cdd9b7ee9a02d7e2749059247494dc1e
Bogdan Plieshka <bogdan.plieshka@zattoo.com>
2024-06-07 12:21:21 +0200

    Test

--- src/index.js
+++ src/index.js
@@ -106,6 +106,9 @@ async function run() {
 
                 if (canPublish) {
                     // await exec.exec(`npm publish ${path}`);
+
+                    throw 'exit';
+
                     core.info(`Published to npm registry: https://www.npmjs.com/package/${name}`);
                 } else {
                     core.info('Package is already exist. Nothing to do here');
